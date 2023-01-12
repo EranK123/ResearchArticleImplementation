58828905753d664e05e52bd39bede82b8d8f4936
erankatz <eranki.katz@gmail.com>
2023-01-12 13:06:46 +0200

    finished website

--- AlgoWebsite.py
+++ AlgoWebsite.py
@@ -66,4 +66,4 @@ def stringlist_to_intlist(str_list):
 
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = 'fnehauildagsukl'
-    app.run(debug=False, host='0.0.0.0', port=5555)
+    app.run(debug=True, host='0.0.0.0', port=5555)
